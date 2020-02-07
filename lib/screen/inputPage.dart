e052f2468ed2bca19c0775627459fe438319df8a
Naxalov <uzbzarif@gmail.com>
2020-02-07 17:05:01 +0900

    Template layot add tow Container

--- lib/screen/inputPage.dart
+++ lib/screen/inputPage.dart
@@ -10,19 +10,41 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title: Text('BMI CALCULATOR'),
+        title: Text('BMI CALCULATOR'),
         centerTitle: true,
       ),
-      body:Container(
+      body: Container(
         child: Column(
           children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ), Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Text('Body'),
             FlatButton(
-              child: Text('input'), onPressed: () {},
+              child: Text('input'),
+              onPressed: () {},
             )
           ],
         ),
       ),
-    ) ;
+    );
   }
 }
