a1353e1fd6aa2f5d49500168b4b00b495dad0973
Naxalov <uzbzarif@gmail.com>
2020-02-07 17:08:25 +0900

    add layout

--- lib/screen/inputPage.dart
+++ lib/screen/inputPage.dart
@@ -45,6 +45,28 @@ class _InputPageState extends State<InputPage> {
                   color: Color(0xFF1D1E33),
                   borderRadius: BorderRadius.circular(10.0)),
             )),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1E33),
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Text('Body'),
             FlatButton(
               child: Text('input'),
