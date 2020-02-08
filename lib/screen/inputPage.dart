29ea0bf9840052e9d069c298b839aec434c4b39d
Naxalov <uzbzarif@gmail.com>
2020-02-08 12:54:44 +0900

    add activeColor

--- lib/screen/inputPage.dart
+++ lib/screen/inputPage.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const Color activeCardColor = Color(0xFF1D1E33);
-
+const Color bottomContainerColor = Color(0xFFEB1555);
+const bottomContainerHeight = 80.0;
 //Color(0xFF1D1E33)
 class InputPage extends StatefulWidget {
   @override
@@ -42,14 +43,14 @@ class _InputPageState extends State<InputPage> {
               ),
             ),
             Expanded(
-              child: InputCard(),
+              child: InputCard(color: activeCardColor,),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(child: InputCard()),
+                  Expanded(child: InputCard(color: activeCardColor,)),
                   Expanded(
-                    child: InputCard(),
+                    child: InputCard(color: activeCardColor,),
                   ),
                 ],
               ),
