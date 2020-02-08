e3cb0573fac2f89d9787139a31f6ae923d1bc666
Naxalov <uzbzarif@gmail.com>
2020-02-08 12:41:13 +0900

    addIconContent widget

--- lib/screen/inputPage.dart
+++ lib/screen/inputPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Color activeCardColor = Color(0xFF8D8E98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -57,6 +60,27 @@ class InputCard extends StatelessWidget {
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
           color: Color(0xFF1D1E33), borderRadius: BorderRadius.circular(10.0)),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
+        )
+      ],
     );
   }
 }
