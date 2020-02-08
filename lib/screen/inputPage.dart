0b593e32eb495780211adb8b2842c15e1d304ccf
Naxalov <uzbzarif@gmail.com>
2020-02-08 13:03:07 +0900

    separate widgites

--- lib/screen/inputPage.dart
+++ lib/screen/inputPage.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/screen/icon_content.dart';
+import 'package:bmi_calculator/screen/input_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -68,46 +70,4 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class InputCard extends StatelessWidget {
-  final Widget cardChild;
-  final Color color;
 
-  InputCard({this.cardChild, this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: color, borderRadius: BorderRadius.circular(10.0)),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  final IconData icon;
-  final String lable;
-
-  IconContent({this.icon, this.lable});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          lable,
-          style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
-        )
-      ],
-    );
-  }
-}
