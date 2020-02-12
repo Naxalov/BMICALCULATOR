7e16c1a4c6a4b3f9e6acf1b7983917b46b2348bf
Naxalov <uzbzarif@gmail.com>
2020-02-12 14:11:14 +0900

    add sliderTheme

--- lib/screen/inputPage.dart
+++ lib/screen/inputPage.dart
@@ -94,18 +94,26 @@ class _InputPageState extends State<InputPage> {
 
                       ],
                     ),
-                    Slider(
-                      value: height.toDouble(),
-                      max: 220,
-                      min: 120,
-                      activeColor: Color(0xFFEB1555),
-                      inactiveColor: Color(0xFF8D8E98),
-                      onChanged: (
-                          double value) {
-                        setState(() {
-                          height = value.round();
-                        });
-                      },),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        activeTrackColor: Colors.white,
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor: Color(0x29EB1555),
+                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0)
+
+                      ),
+                      child: Slider(
+                        value: height.toDouble(),
+                        max: 220,
+                        min: 120,
+                        inactiveColor: Color(0xFF8D8E98),
+                        onChanged: (
+                            double value) {
+                          setState(() {
+                            height = value.round();
+                          });
+                        },),
+                    ),
                   ],
                 ),
                 color: inputActiveCardColor,
